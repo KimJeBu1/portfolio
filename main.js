40ef8e41429c679118be2d4dbf4b20b8cd46177b
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-01-05 17:49:06 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -12,15 +12,24 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle Scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar_menu')
+const navbarMenu = document.querySelector('.navbar_menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
     if(link == null){
         return; //아무것도 하지 않음
     }
-
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    // console.log(event.target.dataset.link);
+    scrollIntoView(link)
 })
+
+// Handle click on "contact me" button home
+const homeContactBtn = document.querySelector('.home_contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact')
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
