ad6161653e3c9d8b5cfa61864053173c45d6574a
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-01-05 16:31:09 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// Handle Scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu')
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return; //아무것도 하지 않음
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
